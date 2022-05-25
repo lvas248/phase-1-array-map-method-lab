b471fe8d9c03461400aa7f9a34f081f511c037ed
Luis <lvas248@gmail.com>
2022-05-24 20:08:13 -0400

    commit tree 3fde1062f4a73fcd32a33714fd9ca92002ee0a1b

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => capitalizeFirstLetter(sentence))
+  }
+ 
+/*
+1) Take tutorial title and identify words:
+- .split(" ")- method that takes an arguments and splits up a string based on argument
+- let newSentence = sentence.split(" ");
+
+2) Capitalize the first letter of each word:
+word = word[0].toUpperCase() + word.substring(1, word.length);
+
+*/
+function capitalizeFirstLetter(sentence){
+  let newSentence = sentence.split(" ");
+  let string = [];
+  for(let word of newSentence){
+    word = word[0].toUpperCase() + word.substring(1, word.length);
+    string = string + word + " ";
+  }
+  return string.trim();
 }
